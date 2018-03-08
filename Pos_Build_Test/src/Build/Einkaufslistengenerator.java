ad53d64a0e84aa34e7878524a5c90d04d1de066f
passe <passe@192.168.1.109>
2018-03-08 10:44:15 +0100

    First commit

---
+++ Pos_Build_Test/src/Build/Einkaufslistengenerator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Einkaufsliste;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ *
+ * @author passe
+ */
+public class Einkaufslistengenerator {
+
+    public void writeList(Einkaufsliste liste, File file) throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(Einkaufsliste.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        m.marshal(liste, file);
+    }
+}
