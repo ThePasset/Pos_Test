be9a6c74b5619a2a85ed225bc0d2ff36a70a1aa6
ThePasset <31887009+ThePasset@users.noreply.github.com>
2018-03-08 10:51:53 +0100

    Update Einkaufslistengenerator.java

--- Pos_Build_Test/src/Build/Einkaufslistengenerator.java
+++ Pos_Build_Test/src/Build/Einkaufslistengenerator.java
@@ -15,7 +15,7 @@ import javax.xml.bind.Marshaller;
  * @author passe
  */
 public class Einkaufslistengenerator {
-
+    //Add a commit to test the build function
     public void writeList(Einkaufsliste liste, File file) throws JAXBException {
         JAXBContext jc = JAXBContext.newInstance(Einkaufsliste.class);
         Marshaller m = jc.createMarshaller();
